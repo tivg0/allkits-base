ee1d6cd0df52f079bda7143cabe927cdb9f59bb2
joaoruirego <joaoruirego2@gmail.com>
2024-05-05 01:23:07 +0100

    adaptar tlm

--- src/app/TextEditor.js
+++ src/app/TextEditor.js
@@ -208,172 +208,174 @@ const TextEditor = forwardRef(
                 <button onClick={handleDelete} className={styles.deleteButton}>
                   <NextImage src={deleteIcon} width={25} height={25} />
                 </button>
-                <div className={styles.fontFamily}>
-                  <div>
-                    <p
-                      style={{
-                        color: "#666",
-                        fontSize: 13,
-                        letterSpacing: -0.8,
-                        marginBottom: 5,
-                        fontFamily: "Inter",
-                      }}
-                    >
-                      Escolha a sua fonte
-                    </p>
+
+                <div className={styles.textHeader}>
+                  <div className={styles.fontFamily}>
                     <div>
-                      <select
+                      <p
                         style={{
-                          backgroundColor: "#f2f2f2",
-                          border: 0,
-                          paddingLeft: 10,
-                          padding: 7,
-                          borderRadius: 100,
-                        }}
-                        className={styles.selectFonts}
-                        value={fontFamily}
-                        onChange={(e) => {
-                          const newFontFamily = e.target.value;
-                          handleFontFamily(newFontFamily);
-                          fabricCanvas.current.renderAll();
-                          updateTexture(); // Apenas se necessário para lógica adicional
+                          color: "#666",
+                          fontSize: 13,
+                          letterSpacing: -0.8,
+                          marginBottom: 5,
+                          fontFamily: "Inter",
                         }}
                       >
-                        {fontList.map((font) => (
-                          <option
-                            key={font}
-                            style={{ fontFamily: font }}
-                            value={font}
-                          >
-                            {font}
-                          </option>
-                        ))}
-                      </select>
+                        Escolha a sua fonte
+                      </p>
+                      <div>
+                        <select
+                          style={{
+                            backgroundColor: "#f2f2f2",
+                            border: 0,
+                            paddingLeft: 10,
+                            padding: 7,
+                            borderRadius: 100,
+                          }}
+                          className={styles.selectFonts}
+                          value={fontFamily}
+                          onChange={(e) => {
+                            const newFontFamily = e.target.value;
+                            handleFontFamily(newFontFamily);
+                            fabricCanvas.current.renderAll();
+                            updateTexture(); // Apenas se necessário para lógica adicional
+                          }}
+                        >
+                          {fontList.map((font) => (
+                            <option
+                              key={font}
+                              style={{ fontFamily: font }}
+                              value={font}
+                            >
+                              {font}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    <p
-                      style={{
-                        color: "#666",
-                        fontSize: 13,
-                        letterSpacing: -0.8,
-                        marginBottom: 5,
-                        fontFamily: "Inter",
-                        marginLeft: 5,
-                      }}
-                    >
-                      Cor do texto
-                    </p>
                     <div>
-                      <select
+                      <p
                         style={{
-                          backgroundColor: "#f2f2f2",
-                          border: 0,
-                          paddingLeft: 10,
-                          padding: 7,
-                          borderRadius: 100,
-                        }}
-                        className={styles.inputSize}
-                        value={fillColor} // Control the selected value with the state
-                        onChange={(e) => {
-                          const newFill = e.target.value;
-                          handleFill(newFill);
-                          updateTexture(); // Aplica a nova fonte ao objeto ativo
+                          color: "#666",
+                          fontSize: 13,
+                          letterSpacing: -0.8,
+                          marginBottom: 5,
+                          fontFamily: "Inter",
+                          marginLeft: 5,
                         }}
                       >
-                        <option value="#feff00">Amerelo Claro</option>
-                        <option value="#88bcec">Azul</option>
-                        <option value="#f8c404">Amarelo</option>
-                        <option value="#000000">Preto</option>
-                        <option value="#90240c">Castanho Avermelhado</option>
+                        Cor do texto
+                      </p>
+                      <div>
+                        <select
+                          style={{
+                            backgroundColor: "#f2f2f2",
+                            border: 0,
+                            paddingLeft: 10,
+                            padding: 7,
+                            borderRadius: 100,
+                          }}
+                          className={styles.inputSize}
+                          value={fillColor} // Control the selected value with the state
+                          onChange={(e) => {
+                            const newFill = e.target.value;
+                            handleFill(newFill);
+                            updateTexture(); // Aplica a nova fonte ao objeto ativo
+                          }}
+                        >
+                          <option value="#feff00">Amerelo Claro</option>
+                          <option value="#88bcec">Azul</option>
+                          <option value="#f8c404">Amarelo</option>
+                          <option value="#000000">Preto</option>
+                          <option value="#90240c">Castanho Avermelhado</option>
 
-                        <option value="#90240c">Verde</option>
-                        <option value="#f0540c">Vermelho Claro</option>
-                        <option value="#1004d4">Azul Escuro</option>
-                        <option value="#08a4d4">Azul Água</option>
-                        <option value="#600c14">Castanho</option>
+                          <option value="#90240c">Verde</option>
+                          <option value="#f0540c">Vermelho Claro</option>
+                          <option value="#1004d4">Azul Escuro</option>
+                          <option value="#08a4d4">Azul Água</option>
+                          <option value="#600c14">Castanho</option>
 
-                        <option value="#48cc3c">Verde Claro</option>
-                        <option value="#d8d49c">Dourado-Bege</option>
-                        <option value="#c8c4c4">Cinza</option>
-                        <option value="#082c0c">Verde Escuro</option>
-                        <option value="#080c1c">Azul-Preto</option>
+                          <option value="#48cc3c">Verde Claro</option>
+                          <option value="#d8d49c">Dourado-Bege</option>
+                          <option value="#c8c4c4">Cinza</option>
+                          <option value="#082c0c">Verde Escuro</option>
+                          <option value="#080c1c">Azul-Preto</option>
 
-                        <option value="#d02414">Vermelho</option>
-                        <option value="#68147c">Violeta</option>
-                        <option value="#ffffff">Branco</option>
-                      </select>
+                          <option value="#d02414">Vermelho</option>
+                          <option value="#68147c">Violeta</option>
+                          <option value="#ffffff">Branco</option>
+                        </select>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <br></br>
-                <div className={styles.alignBtns}>
-                  <button
-                    onClick={() => {
-                      const newAlign = "left";
-                      handleTextAlign(newAlign);
-                    }}
-                    className={styles.alignBtn}
-                  >
-                    <img
-                      src={"./textIconalign.png"}
-                      style={{ width: 20, height: 20, marginTop: 4 }}
-                      alt="Description"
-                    />
-                  </button>
-                  <button
-                    onClick={() => {
-                      const newAlign = "center";
-                      handleTextAlign(newAlign);
-                    }}
-                    className={styles.alignBtn}
-                  >
-                    <img
-                      src={"./textIconalign.png"}
-                      style={{ width: 20, height: 20, marginTop: 4 }}
-                      alt="Description"
-                    />
-                  </button>
-                  <button
-                    onClick={() => {
-                      const newAlign = "right";
-                      handleTextAlign(newAlign);
-                    }}
-                    className={styles.alignBtn}
-                  >
-                    <img
-                      src={"./textIconalign.png"}
-                      style={{ width: 20, height: 20, marginTop: 4 }}
-                      alt="Description"
-                    />
-                  </button>
-                  <button style={{ opacity: 0 }} className={styles.alignBtn}>
-                    <img
-                      src={"./textIconalign.png"}
-                      style={{ width: 20, height: 20, marginTop: 4 }}
-                      alt="Description"
-                    />
-                  </button>
-                  <div style={{ marginLeft: 10 }}>
-                    <p
-                      style={{
-                        color: "#666",
-                        fontSize: 13,
-                        letterSpacing: -0.8,
-                        marginBottom: 0,
-                        fontFamily: "Inter",
+                  <div className={styles.alignBtns}>
+                    <button
+                      onClick={() => {
+                        const newAlign = "left";
+                        handleTextAlign(newAlign);
                       }}
+                      className={styles.alignBtn}
                     >
-                      Tamanho
-                    </p>
-                    <div>
-                      <input
-                        className={styles.inputText}
-                        style={{ width: 90 }}
-                        value={fontSize}
-                        // value={text} // Display text from the active object
-                        onChange={handleSizeChange}
+                      <img
+                        src={"./textIconalign.png"}
+                        style={{ width: 20, height: 20, marginTop: 4 }}
+                        alt="Description"
                       />
+                    </button>
+                    <button
+                      onClick={() => {
+                        const newAlign = "center";
+                        handleTextAlign(newAlign);
+                      }}
+                      className={styles.alignBtn}
+                    >
+                      <img
+                        src={"./textIconalign.png"}
+                        style={{ width: 20, height: 20, marginTop: 4 }}
+                        alt="Description"
+                      />
+                    </button>
+                    <button
+                      onClick={() => {
+                        const newAlign = "right";
+                        handleTextAlign(newAlign);
+                      }}
+                      className={styles.alignBtn}
+                    >
+                      <img
+                        src={"./textIconalign.png"}
+                        style={{ width: 20, height: 20, marginTop: 4 }}
+                        alt="Description"
+                      />
+                    </button>
+                    <button style={{ opacity: 0 }} className={styles.alignBtn}>
+                      <img
+                        src={"./textIconalign.png"}
+                        style={{ width: 20, height: 20, marginTop: 4 }}
+                        alt="Description"
+                      />
+                    </button>
+                    <div style={{ marginLeft: 10 }}>
+                      <p
+                        style={{
+                          color: "#666",
+                          fontSize: 13,
+                          letterSpacing: -0.8,
+                          marginBottom: 0,
+                          fontFamily: "Inter",
+                        }}
+                      >
+                        Tamanho
+                      </p>
+                      <div>
+                        <input
+                          className={styles.inputText}
+                          style={{ width: 90 }}
+                          value={fontSize}
+                          // value={text} // Display text from the active object
+                          onChange={handleSizeChange}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
